44cdbfc35264b7cbcceb4f774c2b5c562d36aa2f
Andrés Piñones Besnier <a01570150@itesm.mx>
2022-05-05 13:49:41 -0500

    final group query

--- data/endpoints/endpointGroupsbyId.sql
+++ data/endpoints/endpointGroupsbyId.sql
@@ -1,4 +1,4 @@
-SELECT Groups.groupID, Groups.name, Endpoint.[methodType], Endpoint.[path]
+SELECT Groups.groupID, Groups.name, Endpoint.[methodType], Endpoint.[endpointDescription]
 FROM [dbo].Groups
 LEFT JOIN dbo.Endpoint ON Groups.groupID = Endpoint.groupID
 WHERE Groups.[apiID] = @apiID  
