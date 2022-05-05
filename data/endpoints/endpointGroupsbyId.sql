0fea2cafd58eedca5c2e008581dad0e0be7d30af
Andrés Piñones Besnier <a01570150@itesm.mx>
2022-05-05 13:54:15 -0500

    query minor fix

--- data/endpoints/endpointGroupsbyId.sql
+++ data/endpoints/endpointGroupsbyId.sql
@@ -1,4 +1,4 @@
-SELECT Groups.groupID, Groups.name, Endpoint.[methodType], Endpoint.[endpointDescription]
+SELECT Groups.groupID, Groups.name, Endpoint.[methodType], Endpoint.[endpointDescription], Endpoint.[endpointID]
 FROM [dbo].Groups
 LEFT JOIN dbo.Endpoint ON Groups.groupID = Endpoint.groupID
 WHERE Groups.[apiID] = @apiID  
